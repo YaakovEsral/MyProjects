a7d37672f7bb39eba2711be83f30ee8f2f2c72b6
Yaakov Esral <yaakovez@gmail.com>
2020-10-26 15:13:20 -0400

    added new json file

--- milim/js/globals.js
+++ milim/js/globals.js
@@ -1,2 +1,5 @@
-let files = [{url: "../json/group1.json", name: 'Group 1'}, {url: "../json/group2.json", name: 'Group 2'}];
+let files = [
+    {url: "../json/group1.json", name: "בבא קמא ב ע\"א"}, 
+    {url: "../json/group2.json", name: "בבא קמא ב ע\"ב"}, 
+    {url: "../json/gimmelBtm.json", name: "בבא קמא ג ע\"א"}];
 
