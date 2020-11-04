df6dccd189eb66d4a91e01a1a3cbec7499163889
Yaakov Esral <yaakovez@gmail.com>
2020-11-03 20:28:00 -0500

    added new words (bk3b)

--- milim/js/globals.js
+++ milim/js/globals.js
@@ -1,5 +1,7 @@
 let files = [
     {url: "../json/group1.json", name: "בבא קמא ב ע\"א"}, 
     {url: "../json/group2.json", name: "בבא קמא ב ע\"ב"}, 
-    {url: "../json/gimmelBtm.json", name: "בבא קמא ג ע\"א"}];
+    {url: "../json/gimmelBtm.json", name: "בבא קמא ג ע\"א"},
+    {url: "../json/bk3b.json", name: "בבא קמא ג ע\"ב"}
+];
 
